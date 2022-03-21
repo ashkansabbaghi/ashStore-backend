b289bfaef9f0d507e0b5f3e95a93edfa27bf3181
ashakn sabbaghi <ashkansabbaghi73@gmail.com>
2022-03-21 14:52:27 +0330

    add token login

--- src/controllers/auth-controllers.js
+++ src/controllers/auth-controllers.js
@@ -1,13 +1,32 @@
 const User = require('../db/models/User')
 const CryptoJS = require('crypto-js')
+const jwt = require('jsonwebtoken')
 
 // REGISTER
 const register = async (req, res) => {
+    const {
+        username,
+        email,
+        password,
+        phone
+    } = req.body
+
+        !(email && username && phone && password) &&
+        res.status(400).json("All input is required") // check empty input
+
+    await User.findOne({
+        username
+    }) && res.status(409).json("This username exists !")
+
+    await User.findOne({
+        email
+    }) && res.status(409).json("There is a user with this email !")
+
     const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
-        phone: req.body.phone
+        username: username,
+        email: email.toLowerCase(),
+        password: CryptoJS.AES.encrypt(password, process.env.PASS_SEC).toString(),
+        phone: phone
     })
 
     try {
@@ -19,7 +38,38 @@ const register = async (req, res) => {
 }
 
 // LOGIN
-const login = async (req, res) => {}
+const login = async (req, res) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username
+        });
+
+        !user && res.status(401).json("Wrong Credentials !") //check existing user
+
+        const hashedPass = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC)
+        const OrgPassword = hashedPass.toString(CryptoJS.enc.Utf8);
+
+        OrgPassword !== req.body.password && res.status(401).json("Wrong Credentials !") //check existing user
+
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        }, process.env.TOKEN_SEC, {
+            expiresIn: "3d"
+        })
+
+        const {
+            password,
+            isAdmin,
+            ...customUser
+        } = user._doc //remove (password ans isAdmin) from user
+
+        res.status(200).json({...customUser,token :accessToken })
+
+    } catch (e) {
+        res.status(500).json(e)
+    }
+}
 
 exports.register = register
 exports.login = login
--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth-controllers')
+const {verifyToken} = require('../controllers/verifyToken')
 
 
 router.post('/register' , authController.register)
-
+router.post('/login' , authController.login)
+router.post('/:id' , verifyToken , (req, res) => {
+    if(req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password , process.env.PASS_SEC).toString()
+    }
+})
 
 module.exports = router
